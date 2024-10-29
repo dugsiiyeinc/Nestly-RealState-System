434c42a3b19de49066bd65c386178a6586f80b55
Abdirahman <Abdirahmannadaara42@gmail.com>
2024-10-29 14:31:44 +0300

    Making Selecting DOM Elements

---
+++ js/About_js/main.js
@@ -0,0 +1,10 @@
+// **Selectng DOM elements
+const authSwitch = document.querySelector("#authSwitch");
+const authButton = document.querySelector("#authButton");
+const switchForm = document.querySelector("#switchForm");
+const formTitle = document.querySelector("#form-title");
+const username = document.querySelector("#username");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirmPassword");
+
