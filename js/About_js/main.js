ffcb97c99f5bb442c5d1e30e09320a3dacbfd0dd
Abdirahman <Abdirahmannadaara42@gmail.com>
2024-10-29 18:05:20 +0300

    Add Warning Alert for Existing User Registration Attempt

--- js/About_js/main.js
+++ js/About_js/main.js
@@ -1,96 +1,106 @@
-// **Selectng DOM elements
-const authSwitch = document.querySelector("#authSwitch");
-const authButton = document.querySelector("#authButton");
-const switchForm = document.querySelector("#switchForm");
-const formTitle = document.querySelector("#form-title");
-const username = document.querySelector("#username");
-const email = document.querySelector("#email");
-const password = document.querySelector("#password");
-const confirmPassword = document.querySelector("#confirmPassword");
 
+const formTitle = document.querySelector('#form-title')
+const authForm = document.querySelector('#authForm')
+const username = document.querySelector('#username')
+const email = document.querySelector('#email')
+const password = document.querySelector('#password')
+const confirmPassword = document.querySelector('#confirmPassword')
+const authButton = document.querySelector('#authButton')
+//* paragraph
+const authSwitch = document.querySelector('#authSwitch')
+//* the link of rigister
+const switchForm = document.querySelector('#switchForm')
+const number = document.querySelector('#number')
 
-let signIn = true;
-// **  Toggle Form
-document.body.addEventListener("click", (e) => {
-    if (e.target.id != "switchForm") return;
-    
-    switchAuthForm();
-  });
-  
-  
-//   ** Set Up Auth Form Submission Event
-const authForm = document.querySelector("#authForm");
+// * click 
+let signIn = true
+document.body.addEventListener('click', (e) => {
+    if (e.target.id != 'switchForm') return;
+    switchFormAuto()
 
-authForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+})
 
-  const user = {
-    username: signIn ? undefined : username.value,
-    email: email.value,
-    password: password.value,
-  };
+///?? form submit with User validation
+authForm.addEventListener('submit', function (e) {
 
-  if (signIn) {
-    const users = JSON.parse(localStorage.getItem("users")) || [];
-    const existingUser = users.find(
-      (user) => user.email === email.value && user.password === password.value
-    );
+    e.preventDefault()
+    const user = {
+        username: signIn ? undefined : username.value,
+        email: email.value,
+        password: password.value,
+        confirmPassword: signIn ? undefined : confirmPassword.value,
+        number: signIn ? undefined : number.value
 
-    if (existingUser) {
-      localStorage.setItem("onlineUser", JSON.stringify(existingUser));
-      window.location.href = '../html/movies.html'
+    }
+    if (signIn) {
+
+        const users = JSON.parse(localStorage.getItem('users')) || []
+
+        const existingUser = users.find(user => user.password == password.value && user.email == email.value)
+        if (existingUser) {
+
+            localStorage.setItem('onlineUser', JSON.stringify(existingUser))
+            //* validate
+            alert('Verified you are in')
+           
+            // window.location.href = `./about.html`
+
+          
+        } else {
+           alert('Invalid credentials')
+        }
+
     } else {
-      alert("Invalid Credentials");
-      return;
-    }
-  } else {
-    const users = JSON.parse(localStorage.getItem("users")) || [];
+        const users = JSON.parse(localStorage.getItem('users')) || []
 
-    const existingUser = users.find(
-      (user) => user.username === username.value && user.email === email.value
-    );
+        const existingUser = users.find(user => user.username == username.value && user.email == email.value)
+        if (existingUser) {
+            Swal.fire({
+                icon: "warning",
+                title: "Oops...",
+                text: `User ${username.value} is already exit`
+                
+            });
+            return;
+        }
 
-    if (existingUser) {
-      alert(`User ${existingUser.username} Already exists`);
-      return;
+        users.push(user)
+        localStorage.setItem('users', JSON.stringify(users));
+        alert('Registaration is successflly')
+       
+        switchFormAuto()
     }
+
+    if (!signIn && confirmPassword.value.trim() !== password.value.trim()) {
+        alert('Passwords do not match')
     
-    if (confirmPassword.value !== password.value) {
-    alert("Password mismatch");
-    return;
-  }
-
-    users.push(user);
-    localStorage.setItem("users", JSON.stringify(users));
-    alert("Registered successfuly");
-    switchAuthForm();
-  }
-
-  
-});
-
-// * Complete Form Toggle Functionality in switchAuthForm
-function switchAuthForm() {
-    signIn = !signIn;
-  
-    if (!signIn) {
-      authButton.textContent = "Sign up";
-      formTitle.textContent = "Sign up";
-      username.style.display = "block";
-      confirmPassword.style.display = "block";
-  
-      authSwitch.innerHTML = `Already have an account? <a href="#" id="switchForm">Sign in </a>`;
-    } else {
-      authButton.textContent = "Sign in";
-      formTitle.textContent = "Sign in";
-      username.style.display = "none";
-      confirmPassword.style.display = "none";
-      username.value = "";
-      confirmPassword.value = "";
-      email.value = "";
-      password.value = "";
-  
-      authSwitch.innerHTML = `Already have an account? <a href="#" id="switchForm">Sign in </a>`;
+        return;
     }
-  }
-  
+
+})
+
+//* switch form autho for sign in and Sign Up
+function switchFormAuto() {
+    signIn = !signIn
+    if (signIn) {
+        formTitle.textContent = 'Sign In'
+        username.style.display = 'none'
+        confirmPassword.style.display = 'none'
+        number.style.display = 'none'
+        authButton.textContent = 'Sign In'
+        username.value = ''
+        confirmPassword.value = ''
+        email.value = ""
+        password.value = ""
+        number.value = ""
+        authSwitch.innerHTML = `New to Nestly? <a href="#" id="switchForm">Register now</a>`
+    } else {
+        formTitle.textContent = 'Sign Up'
+        username.style.display = 'block'
+        confirmPassword.style.display = 'block'
+        authButton.textContent = 'Sign Up'
+        number.style.display = 'block'
+
+        authSwitch.innerHTML = `Already have an account <a href="#" id="switchForm">Sign In</a>`
+    }
+}
