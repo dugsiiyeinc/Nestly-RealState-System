83534dfb11096674137ffca2a56d5142ce4070e2
Faarax <faarax101@gmail.com>
2024-11-05 23:08:21 +0300

    add property statuses to data object in properties.js

--- src/js/properties.js
+++ src/js/properties.js
@@ -6,5 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
       min: ["$1,000", "$5,000", "$10,000", "$15,000", "$20,000"],
       max: ["$50,000", "$75,000", "$100,000", "$150,000", "$200,000"],
     },
+    statuses: [
+      "Newly Listed",
+      "Renovated",
+      "Fully Furnished",
+      "Partially Furnished",
+      "Unfurnished",
+    ],
   };
 });
