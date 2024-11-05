98cb9b41b26b289b660c0be38def6f1cc5489afb
Faarax <faarax101@gmail.com>
2024-11-05 23:06:34 +0300

    add price ranges to data object in properties.js

--- src/js/properties.js
+++ src/js/properties.js
@@ -2,6 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const data = {
     neighborhoods: ["City Center", "Old Town", "Green Acres", "Lakeside"],
     cities: ["San Francisco", "Miami", "Austin", "Seattle"],
-
+    prices: {
+      min: ["$1,000", "$5,000", "$10,000", "$15,000", "$20,000"],
+      max: ["$50,000", "$75,000", "$100,000", "$150,000", "$200,000"],
+    },
   };
 });
