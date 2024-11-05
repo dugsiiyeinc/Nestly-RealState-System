ebd262c8d4637b5fbf8a6f16d1c1cd506e401fb4
Faarax <faarax101@gmail.com>
2024-11-05 23:05:33 +0300

    add cities array to data object in properties.js

--- src/js/properties.js
+++ src/js/properties.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const data = {
     neighborhoods: ["City Center", "Old Town", "Green Acres", "Lakeside"],
+    cities: ["San Francisco", "Miami", "Austin", "Seattle"],
+
   };
 });
